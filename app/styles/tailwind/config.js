53c8177da36947cf577fd69451e6fe7335ca27c3
Jack Howard <jackhowa@fastmail.com>
2022-10-12 18:13:04 -0500

    feat: Add tailwind

---
+++ app/styles/tailwind/config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./app/**/*.hbs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
